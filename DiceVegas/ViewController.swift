a7d54ddb2ad7753b0155287997a76a126d670eb3
rsafin90 <rsafin90@gmail.com>
2022-05-07 17:00:58 +0300

    Initial Commit

---
+++ DiceVegas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceVegas
+//
+//  Created by Руслан Сафин on 07.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
